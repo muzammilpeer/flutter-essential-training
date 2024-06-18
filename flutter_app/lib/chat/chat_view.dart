007b8f88f6e39c5eed3a04fbcc3ce1410870195f
Muzammil Peer <muzammilpeer987@gmail.com>
2024-06-18 11:24:56 +0400

    alignment

--- flutter_app/lib/chat/chat_view.dart
+++ flutter_app/lib/chat/chat_view.dart
@@ -21,126 +21,73 @@ class ChatView extends StatelessWidget {
           ]),
       body: ListView(
         children: [
-          Container(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Text(
-                    "It's your message",
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 10,
-                        color: Colors.white),
-                  ),
-                  Image(
-                      height: 150,
-                      width: 150,
-                      fit: BoxFit.fitHeight,
-                      image: NetworkImage(
-                          "https://3009709.youcanlearnit.net/Alien_LIL_131338.png")),
-                ],
-              ),
-              padding: EdgeInsets.all(cornerRadius),
-              margin: EdgeInsets.all(cornerRadius),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(cornerRadius),
-                      topRight: Radius.circular(cornerRadius),
-                      bottomLeft: Radius.circular(cornerRadius)),
-                  color: Colors.grey)),
-          Container(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Text(
-                    "It's your message",
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 10,
-                        color: Colors.white),
-                  ),
-                  Image(
-                      height: 150,
-                      width: 150,
-                      fit: BoxFit.fitHeight,
-                      image: NetworkImage(
-                          "https://3009709.youcanlearnit.net/Alien_LIL_131338.png")),
-                ],
-              ),
-              padding: EdgeInsets.all(cornerRadius),
-              margin: EdgeInsets.all(cornerRadius),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(cornerRadius),
-                      topRight: Radius.circular(cornerRadius),
-                      bottomLeft: Radius.circular(cornerRadius)),
-                  color: Colors.grey)),
-          Container(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Text(
-                    "It's your message",
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 10,
-                        color: Colors.white),
-                  ),
-                  Image(
-                      height: 150,
-                      width: 150,
-                      fit: BoxFit.fitHeight,
-                      image: NetworkImage(
-                          "https://3009709.youcanlearnit.net/Alien_LIL_131338.png")),
-                ],
-              ),
-              padding: EdgeInsets.all(cornerRadius),
-              margin: EdgeInsets.all(cornerRadius),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(cornerRadius),
-                      topRight: Radius.circular(cornerRadius),
-                      bottomLeft: Radius.circular(cornerRadius)),
-                  color: Colors.grey)),
-          Container(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Text(
-                    "It's your message",
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 10,
-                        color: Colors.white),
-                  ),
-                  Image(
-                      height: 150,
-                      width: 150,
-                      fit: BoxFit.fitHeight,
-                      image: NetworkImage(
-                          "https://3009709.youcanlearnit.net/Alien_LIL_131338.png")),
-                ],
-              ),
-              padding: EdgeInsets.all(cornerRadius),
-              margin: EdgeInsets.all(cornerRadius),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(cornerRadius),
-                      topRight: Radius.circular(cornerRadius),
-                      bottomLeft: Radius.circular(cornerRadius)),
-                  color: Colors.grey)),
-
-
-
-
-
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Container(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      "It's your message",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 10,
+                          color: Colors.white),
+                    ),
+                    Image(
+                        height: 150,
+                        width: 150,
+                        fit: BoxFit.fitHeight,
+                        image: NetworkImage(
+                            "https://3009709.youcanlearnit.net/Alien_LIL_131338.png")),
+                  ],
+                ),
+                padding: EdgeInsets.all(cornerRadius),
+                margin: EdgeInsets.all(cornerRadius),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(cornerRadius),
+                        topRight: Radius.circular(cornerRadius),
+                        bottomLeft: Radius.circular(cornerRadius)),
+                    color: Colors.grey)),
+          ),
+          Align(
+            alignment: Alignment.centerRight,
+            child: Container(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      "It's your message",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 10,
+                          color: Colors.white),
+                    ),
+                    Image(
+                        height: 150,
+                        width: 150,
+                        fit: BoxFit.fitHeight,
+                        image: NetworkImage(
+                            "https://3009709.youcanlearnit.net/Alien_LIL_131338.png")),
+                  ],
+                ),
+                padding: EdgeInsets.all(cornerRadius),
+                margin: EdgeInsets.all(cornerRadius),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(cornerRadius),
+                        topRight: Radius.circular(cornerRadius),
+                        bottomLeft: Radius.circular(cornerRadius)),
+                    color: Colors.grey)),
+          ),
           Container(
             height: 50,
             decoration: BoxDecoration(
-              borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
               color: Colors.black,
             ),
             child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 IconButton(
                     onPressed: () {
@@ -148,7 +95,6 @@ class ChatView extends StatelessWidget {
                     },
                     icon: Icon(Icons.add_a_photo)),
                 Text("Type a message"),
-                Spacer(),
                 IconButton(
                     onPressed: () {
                       print("Send Message");
