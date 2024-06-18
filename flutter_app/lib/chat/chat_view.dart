de15e1fa15a437e618a26a902608c29a67bc8d80
Muzammil Peer <muzammilpeer987@gmail.com>
2024-06-18 11:32:58 +0400

    see how flixable act for loose vs tight for weight

--- flutter_app/lib/chat/chat_view.dart
+++ flutter_app/lib/chat/chat_view.dart
@@ -19,88 +19,100 @@ class ChatView extends StatelessWidget {
                 },
                 icon: Icon(Icons.logout))
           ]),
-      body: ListView(
+      body: Column(
         children: [
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Container(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text(
-                      "It's your message",
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 10,
-                          color: Colors.white),
-                    ),
-                    Image(
-                        height: 150,
-                        width: 150,
-                        fit: BoxFit.fitHeight,
-                        image: NetworkImage(
-                            "https://3009709.youcanlearnit.net/Alien_LIL_131338.png")),
-                  ],
-                ),
-                padding: EdgeInsets.all(cornerRadius),
-                margin: EdgeInsets.all(cornerRadius),
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(cornerRadius),
-                        topRight: Radius.circular(cornerRadius),
-                        bottomLeft: Radius.circular(cornerRadius)),
-                    color: Colors.grey)),
-          ),
-          Align(
-            alignment: Alignment.centerRight,
-            child: Container(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text(
-                      "It's your message",
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 10,
-                          color: Colors.white),
-                    ),
-                    Image(
-                        height: 150,
-                        width: 150,
-                        fit: BoxFit.fitHeight,
-                        image: NetworkImage(
-                            "https://3009709.youcanlearnit.net/Alien_LIL_131338.png")),
-                  ],
-                ),
-                padding: EdgeInsets.all(cornerRadius),
-                margin: EdgeInsets.all(cornerRadius),
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(cornerRadius),
-                        topRight: Radius.circular(cornerRadius),
-                        bottomLeft: Radius.circular(cornerRadius)),
-                    color: Colors.grey)),
-          ),
-          Container(
-            height: 50,
-            decoration: BoxDecoration(
-              color: Colors.black,
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          Flexible(
+            flex: 3,
+            fit: FlexFit.loose,
+            child: ListView(
               children: [
-                IconButton(
-                    onPressed: () {
-                      print("Add attachment");
-                    },
-                    icon: Icon(Icons.add_a_photo)),
-                Text("Type a message"),
-                IconButton(
-                    onPressed: () {
-                      print("Send Message");
-                    },
-                    icon: Icon(Icons.send))
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Container(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text(
+                            "It's your message",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 10,
+                                color: Colors.white),
+                          ),
+                          Image(
+                              height: 150,
+                              width: 150,
+                              fit: BoxFit.fitHeight,
+                              image: NetworkImage(
+                                  "https://3009709.youcanlearnit.net/Alien_LIL_131338.png")),
+                        ],
+                      ),
+                      padding: EdgeInsets.all(cornerRadius),
+                      margin: EdgeInsets.all(cornerRadius),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(cornerRadius),
+                              topRight: Radius.circular(cornerRadius),
+                              bottomLeft: Radius.circular(cornerRadius)),
+                          color: Colors.grey)),
+                ),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: Container(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text(
+                            "It's your message",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 10,
+                                color: Colors.white),
+                          ),
+                          Image(
+                              height: 150,
+                              width: 150,
+                              fit: BoxFit.fitHeight,
+                              image: NetworkImage(
+                                  "https://3009709.youcanlearnit.net/Alien_LIL_131338.png")),
+                        ],
+                      ),
+                      padding: EdgeInsets.all(cornerRadius),
+                      margin: EdgeInsets.all(cornerRadius),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(cornerRadius),
+                              topRight: Radius.circular(cornerRadius),
+                              bottomLeft: Radius.circular(cornerRadius)),
+                          color: Colors.grey)),
+                ),
               ],
+            ),
+          ),
+          Flexible(
+            flex: 1,
+            fit: FlexFit.tight,
+            child: Container(
+              height: 50,
+              decoration: BoxDecoration(
+                color: Colors.black,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                      onPressed: () {
+                        print("Add attachment");
+                      },
+                      icon: Icon(Icons.add_a_photo)),
+                  Text("Type a message"),
+                  IconButton(
+                      onPressed: () {
+                        print("Send Message");
+                      },
+                      icon: Icon(Icons.send))
+                ],
+              ),
             ),
           ),
         ],
